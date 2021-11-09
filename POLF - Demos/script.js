9dfa34cb03ea4576180177e5baa4401a12301358
alexmcdoodz <92785231+alexmcdoodz@users.noreply.github.com>
2021-11-09 18:41:21 +0000

    correct js file

--- POLF - Demos/script.js
+++ POLF - Demos/script.js
@@ -1,108 +1,106 @@
-var Example = Example || {};
 
-Example.slingshot = function() {
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        Runner = Matter.Runner,
-        Composites = Matter.Composites,
-        Events = Matter.Events,
-        Constraint = Matter.Constraint,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        Composite = Matter.Composite,
-        Bodies = Matter.Bodies;
+const vp_width = window.innerWidth / 2, vp_height = window.innerHeight / 2; 
+var Engine = Matter.Engine,
+	World = Matter.World,
+	Bodies = Matter.Bodies;
 
-    // create engine
-    var engine = Engine.create(),
-        world = engine.world;
-
-    // create renderer
-    var render = Render.create({
-        element: document.body,
-        engine: engine,
-        options: {
-            width: 800,
-            height: 600,
-            showAngleIndicator: true
-        }
-    });
-
-    Render.run(render);
-
-    // create runner
-    var runner = Runner.create();
-    Runner.run(runner, engine);
-
-    // add bodies
-    var ground = Bodies.rectangle(395, 600, 815, 50, { isStatic: true, render: { fillStyle: '#060a19' } }),
-        rockOptions = { density: 0.004 },
-        rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
-        anchor = { x: 170, y: 450 },
-        elastic = Constraint.create({ 
-            pointA: anchor, 
-            bodyB: rock, 
-            stiffness: 0.05
-        });
-
-    var pyramid = Composites.pyramid(500, 300, 9, 10, 0, 0, function(x, y) {
-        return Bodies.rectangle(x, y, 25, 40);
-    });
-
-    var ground2 = Bodies.rectangle(610, 250, 200, 20, { isStatic: true, render: { fillStyle: '#060a19' } });
-
-    var pyramid2 = Composites.pyramid(550, 0, 5, 10, 0, 0, function(x, y) {
-        return Bodies.rectangle(x, y, 25, 40);
-    });
-
-    Composite.add(engine.world, [ground, pyramid, ground2, pyramid2, rock, elastic]);
-
-    Events.on(engine, 'afterUpdate', function() {
-        if (mouseConstraint.mouse.button === -1 && (rock.position.x > 190 || rock.position.y < 430)) {
-            rock = Bodies.polygon(170, 450, 7, 20, rockOptions);
-            Composite.add(engine.world, rock);
-            elastic.bodyB = rock;
-        }
-    });
-
-    // add mouse control
-    var mouse = Mouse.create(render.canvas),
-        mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                    visible: false
-                }
-            }
-        });
-
-    Composite.add(world, mouseConstraint);
-
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
-
-    // fit the render viewport to the scene
-    Render.lookAt(render, {
-        min: { x: 0, y: 0 },
-        max: { x: 800, y: 600 }
-    });
-
-    // context for MatterTools.Demo
-    return {
-        engine: engine,
-        runner: runner,
-        render: render,
-        canvas: render.canvas,
-        stop: function() {
-            Matter.Render.stop(render);
-            Matter.Runner.stop(runner);
-        }
-    };
-};
-
-Example.slingshot.title = 'Slingshot';
-Example.slingshot.for = '>=0.14.2';
-
-if (typeof module !== 'undefined') {
-    module.exports = Example.slingshot;
+class ball {
+	// takes in starting location and sets radius of ball
+	constructor(x,y,r) {
+		this.x = x; // Maybe unnecessary
+		this.y = y;
+		this.radius = r;
+		this.diameter = r * 2;
+		// sets the body property of class to a matter.js body
+		this.body = Bodies.circle(this.x, this.y, this.radius); 
+	}
+	// Function to render ball to screen, called within P5 draw()
+	drawBall() {
+		var pos = this.body.position;
+		this.x = pos.x;
+		this.y = pos.y;
+		var angle = this.body.angle;
+		push();
+		translate(pos.x, pos.y);
+		rotate(angle);
+		strokeWeight(1);
+		stroke(255);
+		fill(127);
+		circle(0, 0, this.diameter);
+		pop();
+	}
 }
+
+class ground {
+	constructor (x,y,h,w) {
+		this.x = x
+		this.y = y
+		this.height = h
+		this.width = w
+		this.options = {
+			isStatic: true
+		}
+		this.body = Bodies.rectangle(this.x, this.y, this.height, this.width, this.options)
+	}
+	drawGround() {
+		var pos = this.body.position
+		fill(200)
+		rectMode(CENTER)
+		rect(pos.x, pos.y, this.height, this.width)
+	}
+}
+
+class box {
+	constructor(x,y,h,w) {
+		this.x = x
+		this.y = y 
+		this.height = h
+		this.width = w
+		this.body = Bodies.rectangle(this.x, this.y, this.height, this.width)
+	}
+	drawBox() {
+		var pos = this.body.position;
+		var angle = this.body.angle;
+		push();
+		translate(pos.x, pos.y);
+		rotate(angle);
+		fill(200);
+		rectMode(CENTER)
+		rect(0, 0, this.height, this.width)
+		pop()
+	}
+}
+
+
+
+var boxy, groundy, engine, world, circley, fuck;
+circley = new ball(200, 100, 50);
+groundy = new ground(vp_width / 2, vp_height - 20, 600, 30)
+boxy = new box(200, 10, 100, 100);
+console.log(circley.body)
+console.log(groundy.body)
+
+function setup() {
+	viewport = createCanvas(vp_width, vp_height); 
+	viewport.parent("viewport_container");
+	engine = Engine.create();
+	world = engine.world; 
+	// add our bodies to the world so it is effected by the physics engine. 
+	World.add(world, [boxy.body, circley.body, groundy.body])
+}
+
+
+function draw() {
+	Engine.update(engine)
+	background(255)
+	fill(200)
+	fill(200)
+	boxy.drawBox()
+	circley.drawBall()
+	groundy.drawGround()
+}
+
+// parent class ball, child class be cue ball, potting balls, lead ball.
+// game object - start game, stop game, scores.
+// wall class 
+// course container, has course shape and course elements with their positions. 
