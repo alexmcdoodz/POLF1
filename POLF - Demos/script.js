ea8f7076f6e0a5f569110929039a1c4cde954c01
Charlotte Stick <75076090+charlottestick@users.noreply.github.com>
2021-11-10 13:00:33 +0000

    Brake everything with mouse constraints

--- POLF - Demos/script.js
+++ POLF - Demos/script.js
@@ -2,7 +2,9 @@
 const vp_width = window.innerWidth / 2, vp_height = window.innerHeight / 2; 
 var Engine = Matter.Engine,
 	World = Matter.World,
-	Bodies = Matter.Bodies;
+	Bodies = Matter.Bodies,
+    MouseConstraint = Matter.MouseConstraint,
+    Events = Matter.Events;
 
 class ball {
 	// takes in starting location and sets radius of ball
@@ -29,6 +31,29 @@ class ball {
 		circle(0, 0, this.diameter);
 		pop();
 	}
+}
+
+class cueBall extends ball {
+    constructor(x,y,r) {
+        super(x,y,r);
+    }
+
+    applyForce(mousePos) {
+        // body.applyForce ??
+        // mouse or mouseconstraint might give mouse position and events
+        // Events.on(mouseconstraint, "mousedown", callback) >> event{mouse, sourceElement, eventName}
+
+        // mouse object has mosue.position, docs don't mention it
+
+        // Vector.magnitude(vector)
+        // There should be a maximum magnitude of force, or the user could send the cue to space
+
+        // Force should be applied on mouseup, 
+        // pointer should be drawn while mousedown and position should be updated on mousemove
+
+        force = Vector.create();
+        this.body.applyForce(force);
+    }
 }
 
 class ground {
@@ -73,7 +98,7 @@ class box {
 
 
 
-var boxy, groundy, engine, world, circley, fuck;
+var boxy, groundy, engine, world, circley, mouseConstraint;
 circley = new ball(200, 100, 50);
 groundy = new ground(vp_width / 2, vp_height - 20, 600, 30)
 boxy = new box(200, 10, 100, 100);
@@ -85,19 +110,44 @@ function setup() {
 	viewport.parent("viewport_container");
 	engine = Engine.create();
 	world = engine.world; 
+    mouseConstraint = MouseConstraint.create(engine, {
+        element: viewport,
+        constraint: {}
+    });
 	// add our bodies to the world so it is effected by the physics engine. 
-	World.add(world, [boxy.body, circley.body, groundy.body])
+	World.add(world, [boxy.body, circley.body, groundy.body, mouseConstraint])
 }
 
+
+// mousePos = Mouse.postion
+// force = cueBall.vectorTo(mousePos)
+
+// if mousedown and mousemove then
+//     drawPointer(force)
+
+// if mouseup then
+//     cueBall.applyForce(force)
+//     removePointer()
+var mouseIsDown = false;
+var mousePos;
+
+Events.on(mouseConstraint, "mousedown", function() {
+    mouseIsDown = true;
+    mousePos = mouse.position;
+    console.log(mousePos);
+})
 
 function draw() {
 	Engine.update(engine)
 	background(255)
 	fill(200)
 	fill(200)
+
+
 	boxy.drawBox()
 	circley.drawBall()
 	groundy.drawGround()
+    // Could use ballArray.map((b) => b.drawBall())
 }
 
 // parent class ball, child class be cue ball, potting balls, lead ball.
